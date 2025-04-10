490670411988afeb0a60b1045dabce8925c72eaa
servisbryce <bryceservis@bryceserv.is>
2025-04-10 18:23:33 -0500

    Finished the create_thread_pool function

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -5,6 +5,13 @@
 */
 
 #include <pthread.h>
+#include <stdbool.h>
+
+/*
+
+    This structure defines a task which will be plucked from the thread pool and executed.
+
+*/
 
 typedef struct thread_task {
 
@@ -14,15 +21,22 @@ typedef struct thread_task {
 
 } thread_task_t;
 
+/*
+
+    This structure defines a thread pool. The various mutexes and conditions are defined here along with the statuses of the thread pool.
+
+*/
+
 typedef struct thread_pool {
 
     pthread_mutex_t thread_inactive_threads_mutex;
     pthread_mutex_t thread_active_threads_mutex;
     pthread_mutex_t thread_task_head_mutex;
     pthread_cond_t thread_task_head_condition;
-    pthread_cond_t thread_active_threads_mutex;
+    pthread_cond_t thread_active_threads_condition;
     thread_task_t *thread_task_head;
     size_t inactive_threads;
     size_t active_threads;
+    bool halt;
 
 } thread_pool_t;
--- src/tasks/thread_pool.c
+++ src/tasks/thread_pool.c
@@ -5,3 +5,54 @@
 */
 
 #include "../../include/thread_pool.h"
+#include <stdlib.h>
+
+/*
+
+    Define the thread worker function which envelopes the logic for each thread.
+
+*/
+
+void *thread_worker(void *thread_worker_vargs_p) {
+
+
+
+}
+
+/*
+
+    We aim to create the structure defined in the corresponding thread pool and threads.
+
+*/
+
+thread_pool_t *create_thread_pool(size_t inactive_threads) {
+
+    /* Ensure that our function parameters are valid. */
+    if (inactive_threads < 1) {
+
+        return NULL;
+
+    }
+
+    /* Setup the thread pool structure. */
+    thread_pool_t *thread_pool = (thread_pool_t *) malloc(sizeof(thread_pool_t));
+    pthread_mutex_init(&(thread_pool->thread_inactive_threads_mutex), NULL);
+    pthread_mutex_init(&(thread_pool->thread_active_threads_mutex), NULL);
+    pthread_mutex_init(&(thread_pool->thread_task_head_mutex), NULL);
+    pthread_cond_init(&(thread_pool.->thread_task_head_condition), NULL);
+    pthread_cond_init(&(thread_pool->thread_active_threads_condition), NULL);
+    thread_pool->thread_task_head = NULL;
+    thread_pool->inactive_threads = inactive_threads;
+    thread_pool->active_threads = 0;
+    thread_pool->halt = false;
+    for (size_t i = 0; i < inactive_threads; i++) {
+
+        pthread_t thread;
+        pthread_create(&thread, NULL, NULL, NULL);
+        pthread_detach(&thread);
+
+    }
+
+    return thread_pool;
+
+}
