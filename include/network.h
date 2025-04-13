5e086c50d6a38349a7a6f56d63f394b0c360ed0e
servisbryce <bryceservis@bryceserv.is>
2025-04-12 21:18:08 -0500

    Added comments

--- include/network.h
+++ include/network.h
@@ -1,6 +1,7 @@
 #ifndef NETWORK_H_
 #define NETWORK_H_
 
+/* If you are the worker, you are going to receive this structure in serialized form over the socket. You are to deserialize it and you should find the result to be this structure. */
 typedef struct net_task_request {
 
     size_t identifier;
@@ -9,6 +10,7 @@ typedef struct net_task_request {
 
 } net_task_request_t;
 
+/* You are supposed to fill out this structure, serialize this structure, and then send it over the socket. */
 typedef struct net_task_response {
 
     size_t identifier;
@@ -17,6 +19,7 @@ typedef struct net_task_response {
 
 } net_task_response_t;
 
+/* You are to use this whenever responding to a request so the requestee at least knows that you've acknowledged and received the request. You must also serialize this structure and then send it over the socket. */
 typedef struct net_status {
 
     bool status;
