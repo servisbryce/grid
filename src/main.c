626eb6beed8851099063e64abcfaad77c1ccc1ef
servisbryce <bryceservis@bryceserv.is>
2025-04-19 18:54:36 -0500

    Added certificate and certificate key handling

--- src/main.c
+++ src/main.c
@@ -3,30 +3,43 @@
 #include "../include/network.h"
 #include "../include/serialization.h"
 #include "../include/base64.h"
+#include "../include/tls.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/mman.h>
-#include <stdint.h>
-#include <dlfcn.h>
 
-void *print(void *data) {
+int main(int argc, char **argv) {
 
-    printf("da\n");
-    printf("how are ya doin?\n");
-    printf("change412\n");
-    printf("hi\n");
-    int a = 0;
-    a++;
-    return data;
+    /* Parse all command-line arguments. */
+    arguments_t arguments = parse_arguments(argc, argv);
+    
+    /* See if Transport Layer Security certificates and keys are specified. */
+    SSL_CTX *ssl_context = NULL;
+    if (arguments.tls_certificate && arguments.tls_certificate_key) {
 
-}
+        /* If they are specified, we should create an appropriate context. */
+        if (arguments.workerMode) {
 
-void main(int argc, char **argv) {
+            /* Create a server context. */
+            ssl_context = create_ssl_server_context(arguments.tls_certificate, arguments.tls_certificate_key);
+            if (ssl_context == NULL) {
 
-    arguments_t args = parse_arguments(argc, argv);
-    printf("%ld\n", args.threads);
-    printf("%s\n", args.tls_certificate);
-    printf("%s\n", args.tls_certificate_key);
+                fprintf(stderr, "Failed to create SSL context for server.\n");
+                return -1;
+
+            }
+
+        } else {
+
+            /* Create a client context. */
+            ssl_context = create_ssl_client_context(arguments.tls_certificate, arguments.tls_certificate_key);
+            if (ssl_context == NULL) {
+
+                fprintf(stderr, "Failed to create SSL context for client.\n");
+                return -1;
+
+            }
+
+        }
+
+    }
 
 }
