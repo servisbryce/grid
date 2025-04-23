456c036aa2c8451d8e4ea8525f99e545196650e0
servisbryce <bryceservis@bryceserv.is>
2025-04-23 17:51:05 -0500

    Added function signatures

--- include/sequencer.h
+++ include/sequencer.h
@@ -7,4 +7,12 @@
 #ifndef SEQUENCER_H
 #define SEQUENCER_H
 
+#include <stddef.h>
+
+/* We aim to provide a function to disassemble any message into multiple different messages sized the same as or below the packet length. */
+char **disassemble_message(char *message, size_t packet_length);
+
+/* We aim to provide a function to assemble multiple messages into a single message. */
+char *reassemble_message(char **messages);
+
 #endif
--- src/interfaces/network/sequencer.c
+++ src/interfaces/network/sequencer.c
@@ -4,3 +4,18 @@
 
 */
 
+#include "../../../include/sequencer.h"
+
+/* We aim to provide a function to disassemble any message into multiple different messages sized the same as or below the packet length. */
+char **disassemble_message(char *message, size_t packet_length) {
+
+
+
+}
+
+/* We aim to provide a function to assemble multiple messages into a single message. */
+char *reassemble_message(char **messages) {
+
+
+
+}
