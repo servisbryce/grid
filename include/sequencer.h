1eee16982a1d3750f01e8a2f7c503599b4dc544e
servisbryce <bryceservis@bryceserv.is>
2025-04-23 17:46:40 -0500

    Added sequencer.c and sequencer.h

---
+++ include/sequencer.h
@@ -0,0 +1,10 @@
+/* 
+
+    We aim to provide a facility to disassemble and assemble messages so that they'll fit over a socket (standard maximum length of 64 kilobytes per message).
+
+*/
+
+#ifndef SEQUENCER_H
+#define SEQUENCER_H
+
+#endif
---
+++ src/interfaces/network/sequencer.c
@@ -0,0 +1,6 @@
+/* 
+
+    We aim to provide a facility to disassemble and assemble messages so that they'll fit over a socket (standard maximum length of 64 kilobytes per message).
+
+*/
+
