d70c225b768ddc23b31fdf4cf01fe2baeb5e03e5
servisbryce <bryce@bryceserv.is>
2025-05-03 18:49:41 -0500

    Fixed comments and debug messages.

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv) {
 
     /* Parse all command-line arguments. */
     arguments_t arguments = parse_arguments(argc, argv);
-    
+
     /* See if Transport Layer Security certificates and keys are specified. */
     SSL_CTX *ssl_context = NULL;
     if (arguments.tls_certificate && arguments.tls_certificate_key) {
@@ -21,22 +21,22 @@ int main(int argc, char **argv) {
         /* If they are specified, we should create an appropriate context. */
         if (arguments.workerMode) {
 
-            /* Create a server context. */
+            /* Create a client context. */
             ssl_context = create_ssl_client_context(arguments.tls_certificate, arguments.tls_certificate_key);
             if (ssl_context == NULL) {
 
-                fprintf(stderr, "Failed to create SSL context for server.\n");
+                fprintf(stderr, "Failed to create SSL context for client.\n");
                 return EXIT_FAILURE;
 
             }
 
         } else {
 
-            /* Create a client context. */
+            /* Create a server context. */
             ssl_context = create_ssl_server_context(arguments.tls_certificate, arguments.tls_certificate_key);
             if (ssl_context == NULL) {
 
-                fprintf(stderr, "Failed to create SSL context for client.\n");
+                fprintf(stderr, "Failed to create SSL context for server.\n");
                 return EXIT_FAILURE;
 
             }
